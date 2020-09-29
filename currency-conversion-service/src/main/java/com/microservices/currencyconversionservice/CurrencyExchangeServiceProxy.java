332fe10e1c3bf907780e7271728466c6777fe231
Diego Ramirez <diegoramirez1594@gmail.com>
2020-09-29 15:26:47 -0500

    Ribbon configuration

--- currency-conversion-service/src/main/java/com/microservices/currencyconversionservice/CurrencyExchangeServiceProxy.java
+++ currency-conversion-service/src/main/java/com/microservices/currencyconversionservice/CurrencyExchangeServiceProxy.java
@@ -1,12 +1,14 @@
 package com.microservices.currencyconversionservice;
 
 import com.microservices.currencyconversionservice.bean.CurrencyConversionBean;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "currency-exchange-service",url = "localhost:8000")
+@FeignClient(name = "currency-exchange-service")
+@RibbonClient(name = "currency-exchange-service")
 public interface CurrencyExchangeServiceProxy {
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public ResponseEntity<CurrencyConversionBean> retrieveExchangeValue(@PathVariable("from") String from,
